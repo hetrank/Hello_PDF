eb53b6342e31711dfadf854d7440a1fd1b762fdd
hetrank <hetrank@gmail.com>
2026-04-08 11:14:42 +0530

    Created the generation file and its test file.

--- generation.py
+++ generation.py
@@ -1,1 +1,71 @@
-#yet to be developed
+from langchain_groq import  ChatGroq
+from langchain_core.prompts import PromptTemplate
+
+
+# LLM:
+llm = ChatGroq(
+  model="llama-3.3-70b-versatile",
+  temperature=0
+)
+
+
+# Format Documents:
+def format_docs(docs):
+  formatted = []
+
+  for doc in docs:
+    source = doc.metadata.get("source", "unknown")
+    page = doc.metadata.get("page", "unknown")
+
+    chunk = f"[Source: {source}, Page {page}]\n{doc.page_content}"
+    formatted.append(chunk)
+  
+  return "\n\n".join(formatted)
+
+
+# Prompt
+PROMPT = PromptTemplate(
+  input_variables=["context", "question"],
+  template="""
+You are a helpful research assistant.
+
+Use ONLY the provided context to answer the question.
+
+For every statement you make, include a citation in this format:
+[Source: filename, Page X]
+
+If the answer is not in the context, say:
+"I could not find the answer in the provided documents."
+
+Be concise. Prefer bullet points where possible.
+
+--------------------------------------
+context:
+{context}
+--------------------------------------
+
+Question:
+{question}
+
+Answer:
+"""
+)
+
+
+# Generate Answer
+def generate_answer(retriever, query):
+  docs = retriever.invoke(query)
+
+  if not docs:
+    return "No relevant documents found."
+  
+  context = format_docs(docs)
+
+  prompt = PROMPT.format(
+    context = context,
+    question = query
+  )
+
+  response = llm.invoke(prompt)
+
+  return response.content
---
+++ test_generation.py
@@ -0,0 +1,22 @@
+from rag_retriever import get_retriever
+from generation import generate_answer
+import time
+
+start = time.time()
+
+start_retr = time.time()
+retriever = get_retriever()
+end_retr = time.time()
+print(f"Got the retriever in {end_retr-start_retr}")
+
+query = "What is the offenders name?"
+
+start_ans = time.time()
+answer = generate_answer(retriever, query)
+end_ans = time.time()
+print(f"Got the ans in {end_ans-start_ans}")
+
+print(answer)
+
+end = time.time()
+print(f"Time to test entire generation : {end-start}")
